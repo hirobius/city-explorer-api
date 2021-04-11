c8e86fdf97919c9e86c84afcf5976db6b682c517
Adrian Milsap <adrian@hirobius.com>
2021-04-11 14:40:19 -0700

    data logging but not rendering

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 require('dotenv').config();
 // import the json data file
 const weatherData = require('./data/weather.json');
+const cors = require('cors');
 
 const app = express();
+
+// make sure your data is accessible from the React frontend
+app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 // response has some methods that are very helpful. i.e. send
