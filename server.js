a9bde87d5da6bf7c849cd14a90ec24451a568774
Adrian Milsap <adrian@hirobius.com>
2021-04-11 12:46:42 -0700

    json loaded and returning

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 // actually use the .env file created
 require('dotenv').config();
+// import the json data file
+const weatherData = require('./data/weather.json');
 
 const app = express();
 
 const PORT = process.env.PORT || 3002;
-
+// response has some methods that are very helpful. i.e. send
 app.get('/', (request, response) => {
   response.send('Hiya!');
+});
+
+app.get('/weather', (request, response) => {
+  response.json(weatherData);
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
