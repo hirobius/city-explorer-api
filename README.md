5fe86408a1a586f70de589675f821f28fafaeb45
Adrian Milsap <adrian@hirobius.com>
2021-04-18 13:16:42 -0700

    working API call to weatherbit

--- README.md
+++ README.md
@@ -23,15 +23,65 @@
 
 ## Credit and Collaborations
 
-No collaborators.
+Marie Marcos on the Whiteboard Request Response Cycle.
 
---- 
+Michelle's review of Prabin's Lab-08 code!!!
+
+---
 
 ## Time Estimates
 
-Name of feature: ________________________________
+Name of feature: Access the query parameters from the web client request object, to identify the exact location for which the web client is requesting weather info.
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: __1hr___
+
+Start time: __830___
+
+Finish time: __935___
+
+Actual time needed to complete: __1hr5m___
+
+---
+
+Name of feature: Update the callback for your `/weather` route so that, instead of returning data from the `weather.json` file, it makes a Superagent request to the weather API with the latitude and longitude from the web client.
+
+Estimate of time needed to complete: __45m___
+
+Start time: __0940___
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+---
+
+Name of feature: Use your constructor function to shape the API results.
+
+Estimate of time needed to complete: __1hr___
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+---
+
+Name of feature: Respond to the web client with an array of objects corresponding to each day in the API data. See the sample response.
+
+Estimate of time needed to complete: __1hr___
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+---
+
+Name of feature: Ensure your web client still renders the weather data correctly.
+
+Estimate of time needed to complete: __1hr___
 
 Start time: _____
 
--- server.js
+++ server.js
@@ -2,25 +2,18 @@
 
 // import the json data file
 const weatherData = require('./data/weather.json');
-
 // actually use the .env file created
 require('dotenv').config();
-
 // include express for the express server (require is pretty much the backend import)
 const express = require('express');
-
 const cors = require('cors');
-
 const { response } = require('express');
-
 // instantiate the express server
 const app = express();
-
 // make sure your data is accessible from the React frontend
 app.use(cors());
-
 const superagent = require('superagent');
-
+// process.env is how you pull the PORT variable from the .env file
 const PORT = process.env.PORT || 3002;
 
 app.get('/', (request, response) => {
@@ -36,24 +29,6 @@ function handleErrors() {
   response.status(500).send('Internal error :/');
 }
 
-// http://localhost:3001/weather?lat=47.6062&long=-122.3321
-app.get('/weather', (request, response) => {
-  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
-    .query({
-      key: process.env.WEATHER_API_KEY,
-      lat: request.query.lat,
-      lon: request.query.lon
-    })
-    .then(weatherData => {
-      console.log(weatherData.body.city_name)
-      response.json(weatherData.body.data.map(x => (
-        {
-          date: x.valid_date,
-          description: x.weather.description
-        })));
-    });
-});
-
 // app.get('/weather', (request, response) => {
 //   try {
 //     const allForecasts = weatherData.data.map(day => new Forecast(day));
@@ -62,5 +37,27 @@ app.get('/weather', (request, response) => {
 //     handleErrors(errors, response);
 //   }
 // });
+
+app.get('/weather', (request, response) => {
+  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
+  .query({
+    key: process.env.WEATHER_API_KEY,
+    units: 'I',
+    lat: request.query.lat,
+    lon: request.query.lon
+  })
+  .then(weatherData => {
+    console.log(weatherData.body.city_name);
+    response.send(weatherData.body.data.map(x => (
+      {date: x.valid_date,
+      description: x.weather.description})))
+  });
+  // try {
+  //   const allForecasts = weatherData.data.map(day => new Forecast(day));
+  //   response.json(allForecasts);
+  // } catch (error) {
+  //   handleErrors(errors, response);
+  // }
+});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
