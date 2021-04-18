0b77737053169671394dab37d691d39174c53e8f
Adrian Milsap <adrian@hirobius.com>
2021-04-18 09:34:17 -0700

    properly accessing weatherbit API

--- server.js
+++ server.js
@@ -2,18 +2,25 @@
 
 // import the json data file
 const weatherData = require('./data/weather.json');
+
 // actually use the .env file created
 require('dotenv').config();
+
 // include express for the express server (require is pretty much the backend import)
 const express = require('express');
+
 const cors = require('cors');
+
 const { response } = require('express');
+
 // instantiate the express server
 const app = express();
+
 // make sure your data is accessible from the React frontend
 app.use(cors());
+
 const superagent = require('superagent');
-// process.env is how you pull the PORT variable from the .env file
+
 const PORT = process.env.PORT || 3002;
 
 app.get('/', (request, response) => {
@@ -29,44 +36,31 @@ function handleErrors() {
   response.status(500).send('Internal error :/');
 }
 
+// http://localhost:3001/weather?lat=47.6062&long=-122.3321
 app.get('/weather', (request, response) => {
-  try {
-    const allForecasts = weatherData.data.map(day => new Forecast(day));
-    response.json(allForecasts);
-  } catch (error) {
-    handleErrors(errors, response);
-  }
+  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
+    .query({
+      key: process.env.WEATHER_API_KEY,
+      lat: request.query.lat,
+      lon: request.query.lon
+    })
+    .then(weatherData => {
+      console.log(weatherData.body.city_name)
+      response.json(weatherData.body.data.map(x => (
+        {
+          date: x.valid_date,
+          description: x.weather.description
+        })));
+    });
 });
 
-// // get images fom upsplash api
-// app.get('./images', getImages);
-
-// // upsplash callback function 
-// function getImages(request, response) {
-//   // console.log(request.query.imageSearch);
-//   const imageQuery = 'bees'; // replace bees with request.query.imageSearch
-//   const url = 'https://api.unsplash.com/search/photos';
-//   const query = {
-//     client_id: process.env.UNSPLASH_API_KEY,
-//     query: imageQuery,
+// app.get('/weather', (request, response) => {
+//   try {
+//     const allForecasts = weatherData.data.map(day => new Forecast(day));
+//     response.json(allForecasts);
+//   } catch (error) {
+//     handleErrors(errors, response);
 //   }
-//   superAgent
-//     .get(url)
-//     .query(query)
-//     .then(imageResults => {
-//       response.status(200).send(imageResults.body.results.map(img => new ImageObject(img)));
-//     })
-// }
-
-// things I need from
-// description
-// urls.regular
-// user.name
-
-// function ImageObject(img){
-//   this.alt = img.alt.description;
-//   this.url = img.urls.regular;
-//   this.photographer = img.user.name;
-// }
+// });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
