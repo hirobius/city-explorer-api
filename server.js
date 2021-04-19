569ad81ce6db8d1d61a3c6373ba5171a50c37cf8
Adrian Milsap <adrian@hirobius.com>
2021-04-19 10:04:23 -0700

    backend corrections based on review

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// const weatherData = require('./data/weather.json');
+const weatherDataTest = require('./data/weather.json');
 
 const express = require('express');
 require('dotenv').config();
@@ -19,41 +19,41 @@ function handleErrors() {
   response.status(500).send('Internal error :/');
 }
 
-// function Forecast(day, description) {
-//   this.date = day.datetime;
-//   this.description = day.weather.description;
-// }
+function Forecast(day, description) {
+  this.date = day.datetime;
+  this.description = day.weather.description;
+}
 
-// app.get('/weather', (request, response) => {
-//   try {
-//     const allForecasts = weatherData.data.map(day => new Forecast(day));
-//     response.json(allForecasts);
-//   } catch (error) {
-//     handleErrors(errors, response);
-//   }
-// });
+app.get('/weatherTest', (request, response) => {
+  try {
+    const allForecasts = weatherDataTest.data.map(day => new Forecast(day));
+    response.json(allForecasts);
+  } catch (error) {
+    handleErrors(errors, response);
+  }
+});
+
+// test link
+// https://api.weatherbit.io/v2.0/forecast/daily?key=d11d348306704f6c860157d5a9607221&lat=37.1801529&lon=-89.3502834
 
 app.get('/weather', (request, response) => {
-  superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${request.query.lat}&lon=${request.query.lon}`)
+  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
     .query({
-      cityName: request.query.cityName,
-      key: process.env.WEATHER_API_KEY,
-      lat: request.query.lat,
-      lon: request.query.lon
+      //   cityName: request.query.cityName,
+      key: process.env.WEATHERBIT_API_KEY,
+      lat: request.query.lat, // '37.1801529',
+      lon: request.query.lon // '-89.3502834'
     })
     .then(weatherData => {
-      // console.log(weatherData.body.city_name);
       response.json(weatherData.body.data.map(x => (
         {
-          date: x.valid_date,
+          date: x.datetime,
           description: x.weather.description
-        })))
+        })));
     });
 });
 
 // app.get('/movies', (request, response) => {
 //   superagent.get(`https://api.themoviedb.org/3/movie/550?api_key=${MOVIES_API_KEY}`)
-  
-// })
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
