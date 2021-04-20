9c4efb3d3666fbd6492e38d4054f25ff2264b6fc
Adrian Milsap <adrian@hirobius.com>
2021-04-19 18:14:26 -0700

    working movies API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const weatherDataTest = require('./data/weather.json');
+// const weatherDataTest = require('./data/weather.json');
+// const movieDataTest = require('.data/movie.json');
 
 const express = require('express');
 require('dotenv').config();
@@ -24,16 +25,17 @@ function Forecast(day) {
   this.description = day.weather.description;
 }
 
-app.get('/weatherTest', (request, response) => {
-  try {
-    const allForecasts = weatherDataTest.data.map(day => new Forecast(day));
-    response.json(allForecasts);
-  } catch (error) {
-    handleErrors(errors, response);
-  }
-});
+function Movie(movie) {
+  this.title = movie.title;
+  this.overview = movie.overview;
+  this.average_votes = movie.vote_average;
+  this.total_votes = movie.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  this.popularity = movie.popularity;
+  this.released_on = movie.release_date
+}
 
-// test link
+// weather test link
 // https://api.weatherbit.io/v2.0/forecast/daily?key=d11d348306704f6c860157d5a9607221&lat=37.1801529&lon=-89.3502834
 
 app.get('/weather', (request, response) => {
@@ -41,8 +43,8 @@ app.get('/weather', (request, response) => {
     .query({
       cityName: request.query.cityName,
       key: process.env.WEATHERBIT_API_KEY,
-      lat: request.query.lat, // '37.1801529',
-      lon: request.query.lon // '-89.3502834'
+      lat: request.query.lat,
+      lon: request.query.lon,
     })
     .then(weatherData => {
       response.json(weatherData.body.data.map(day => (
@@ -50,7 +52,22 @@ app.get('/weather', (request, response) => {
     });
 });
 
-// app.get('/movies', (request, response) => {
-//   superagent.get(`https://api.themoviedb.org/3/movie/550?api_key=${MOVIES_API_KEY}`)
+// movie test link
+// https://api.themoviedb.org/3/movie/550?api_key=da385cb10f2b51116039cda212b431c5
+// 550 is fightclub
+
+app.get('/movies', (request, response) => {
+  console.log(request.query.cityName)
+  superagent.get('https://api.themoviedb.org/3/search/movie/')
+    .query({
+      api_key: process.env.MOVIES_API_KEY,
+      query: request.query.cityName
+    })
+    .then(movieData => {
+      console.log(movieData.body); // king console log
+      response.json(movieData.body.results.map(movie => (
+        new Movie(movie))));
+    });
+});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
